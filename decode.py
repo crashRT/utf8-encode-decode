56bd06aaa6942c6a510dd15821213dfeb8e741fe
crashRT <85057127+crashRT@users.noreply.github.com>
2022-10-13 12:58:01 +0900

    test

--- decode.py
+++ decode.py
@@ -11,6 +11,6 @@ def decodeFromUTF8(utf8_string):
     return hex.decode('utf-8')
     
 
-source = 'うわああ'
+source = 'hogehogeにゃーん'
 print(decodeFromUTF8(encodeToUTF8(source)))
 print(decodeFromUTF8('E3 81 AB E3 82 83 E3 83 BC E3 82 93'))
