c75a572ca769e83046f7a6b97418a931a55c6527
crashRT <85057127+crashRT@users.noreply.github.com>
2022-10-13 17:36:27 +0900

    fix

--- test.py
+++ test.py
@@ -2,12 +2,11 @@ from encode import *
 from decode import *
 
 
-
 source = 'hogehogeにゃーん'
 
 # encode
 print(encodeToUTF8(source))
-    
+
 # decode
 print(decodeFromUTF8(encodeToUTF8(source)))
 print(decodeFromUTF8('E3 81 AB E3 82 83 E3 83 BC E3 82 93'))
